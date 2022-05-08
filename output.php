49b3c383267e9eea6ce0b313f13d83fde5fbde3c
aqila99 <aqilahazira1@gmail.com>
2022-05-08 17:27:40 +0800

    commit

---
+++ output.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <?php
+        $color = "red";
+        ?>
+        <p>Roses are <?=$color?></p></body>
+        <html>
---
+++ string.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <?php
+        $txt1 = "Hello there, World!";
+        $txt2 = "Have a nice day!";
+        $greet = $txt1."".$txt2;
+        echo $greet;?>
+        echo "<br/>"; 
+        strpos("Hello World!");
+        echo strpos("Hello world!", "world");
+        echo "<br/>";
+        ?>
+</body>
+</htm>
+        
